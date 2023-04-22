3aaff1165a4be3079eba6f95e5a086a98c67fcf5
MrFairyDragon <60883151+MrFairyDragon@users.noreply.github.com>
2023-04-22 20:28:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,80 @@
+public class Main {
+    public static void main(String[] args) {
+        if(args.length < 3) {
+            System.out.println("Please provide atleast 3 arguments");
+        }
+        if(!isInteger(args[0], 10)) {
+            System.out.println("Please provide a whole number no decimals as first parameter");
+        }
+        if(!isFloat(args[1], 10)) {
+            System.out.println("Please provide a number as a second parameter");
+        }
+        ItemType type = null;
+        try {
+            type = ItemType.valueOf(args[2].toUpperCase());
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Third parameter must be of a valid type (Book, Online)");
+        }
+        int amount = Integer.parseInt(args[0]);
+        float price = Float.parseFloat(args[1]);
+        if(type != null) {
+            System.out.println("The final price is: "+calcPrice(amount, price, type)+" DKK");
+        }
+    }
+
+    private static float calcFreight(int amount) {
+        if(amount <= 10) {
+            return 50f;
+        } else {
+            return 50f + ((amount/10)*25);
+        }
+    }
+
+    private static float calcPrice(int amount, float price, ItemType type) {
+        switch (type) {
+            case ONLINE -> {return price*amount;}
+            case BOOK -> {return calcFreight(amount)+(price*amount);}
+        }
+        System.out.println("Error invalid ItemType");
+        return price;
+    }
+
+    public static boolean isInteger(String numberString, int radix) {
+        if(numberString.isEmpty()) {
+            return false;
+        }
+        for(int i = 0; i < numberString.length(); i++) {
+            if (i == 0 && numberString.charAt(i) == '-') {
+                if (numberString.length() == 1) {
+                    return false;
+                } else continue;
+            }
+            if (Character.digit(numberString.charAt(i), radix) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isFloat(String numberString, int radix) {
+        if(numberString.isEmpty()) {
+            return false;
+        }
+        boolean decimal = true;
+        for(int i = 0; i < numberString.length(); i++) {
+            if (i == 0 && numberString.charAt(i) == '-') {
+                if (numberString.length() == 1) {
+                    return false;
+                } else continue;
+            }
+            if(i != 0 && numberString.charAt(i) == '.' && decimal) {
+                decimal = false;
+                continue;
+            }
+            if (Character.digit(numberString.charAt(i), radix) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
